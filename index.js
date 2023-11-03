c801b7c8f59cfe4eaa6264daad0e3e10a84017b7
othmaneelbelghyty <othmane.elbelghyty@gmail.com>
2023-11-03 15:13:18 +0100

    Functio-Factory

---
+++ index.js
@@ -0,0 +1,15 @@
+//Return the Sum of Two Numbers
+function addition(a, b) {
+  return a + b;
+}
+
+// Function Factory
+function makePlusFunction(baseNum) {
+  return function (num) {
+    return baseNum + num;
+  };
+}
+Return - the - Sum - of - Two - Numbers;
+function addition(a, b) {
+  return a + b;
+}
