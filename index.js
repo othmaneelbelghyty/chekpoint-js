64ffe020baa19b826a7b342e77a94e6f961630fe
othmaneelbelghyty <othmane.elbelghyty@gmail.com>
2023-11-03 15:27:41 +0100

    Function-getGrade

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ Return - the - Sum - of - Two - Numbers;
 function addition(a, b) {
   return a + b;
 }
+//Function get Grade
+function getGrade(s1, s2, s3) {
+  let score = (s1 + s2 + s3) / 3;
+
+  if ((score >= 90) & (score <= 100)) {
+    return "A";
+  } else if ((score >= 80) & (score < 90)) {
+    return "B";
+  } else if ((score >= 70) & (score < 80)) {
+    return "C";
+  } else if ((score >= 60) & (score < 70)) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+console.log("=============>");
